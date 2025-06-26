d92bbe7bab83eddc4fdb2657eb23efbce64dba8f
Moonmowa <k.chandradeep369@gmail.com>
2025-06-26 23:04:15 +0530

    has router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Work from './components/Work/Work';
 import Articles from './components/Articles/Articles';
 
 function App() {
   return (
-    
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
