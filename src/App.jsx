8b2987eaedb9be973eb3e0fec0296d112d610fa3
Moonmowa <k.chandradeep369@gmail.com>
2025-07-04 00:50:16 +0530

    Has based routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Articles from './components/Articles/Articles';
 import Navbar from './components/common/NavBar/Navbar';
